4bd38e48499fed31bc63811c1eba7c4c0b93ceb7
Stefan0114 <99846139+Stefan0114@users.noreply.github.com>
2022-08-09 14:04:25 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -6,14 +6,15 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+song1_status = "";
+song2_status = "";
 
-function preload()
-{
+function preload() {
     song = loadSound("music.mp3");
+    song1 = loadSound("music2.mp3");
 }
 
-function setup()
-{
+function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
 
@@ -24,53 +25,60 @@ function setup()
     poseNet.on('pose', gotPoses);
 }
 
-function draw()
-{
+function draw() {
     image(video, 0, 0, 600, 500);
-    
-    song1_status = song1.isPlaying(); song2_status = song2.isPlaying(); fill("#FF0000"); stroke("#FF0000");
 
-    if(scoreRightWrist > 0.2) 
-    {
-         circle(rightWristX,rightWristY,20); 
-         song2.stop(); 
-         
-         if(song1_status == false) 
-         { 
-            song1.play(); 
-            document.getElementById("song").innerHTML = "Playing - Harry Potter Theme Song" 
-        } 
-}
+    song1_status = song.isPlaying();
+    song2_status = song1.isPlaying();
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if (scoreRightWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        song.stop();
+
+        if (song1_status == false) {
+            song1.play();
+            document.getElementById("song").innerHTML = "Playing - Harry Potter Theme Song"
+        }
+    }
+
+    if (scoreLeftWrist > 0.2) {
+        
+        circle(leftWristX, leftWristY, 20);
+        song1.stop();
+        
+        if (song2_status == false) {
+            song.play();
+            document.getElementById("song").innerHTML = "Playing - Peter Pan Song"
+        }
+    }
 }
 
-function play1()
-{
+function play1() {
     song.play();
     play2.stop()
     song.setVolume(1);
     song.rate(1);
 }
 
-function play2()
-{
+function play2() {
     song.play();
     play1.stop()
     song.setVolume(1);
     song.rate(1);
 }
 
-function modelLoaded()
-{
+function modelLoaded() {
     console.log('PoseNet Is Initialized');
 }
 
-function gotPoses(results)
-{
-    if(results.length > 0)
-    {
+function gotPoses(results) {
+    if (results.length > 0) {
         console.log(results);
         scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
